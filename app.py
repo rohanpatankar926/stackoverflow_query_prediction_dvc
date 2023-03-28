bf6c20571f9bd65a3b0c0c4ec99ce6d552c4b563
rohanpatanka926 <rohanpatankar926@gmail.com>
2023-03-28 22:21:35 +0530

    updated

--- app.py
+++ app.py
@@ -9,7 +9,5 @@ app=Flask(__name__)
 def index():
     return jsonify({"message":"This is the success message"})
 
-
-
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=8000,debug=False)
--- get_data.py
+++ get_data.py
@@ -1,10 +1,10 @@
 import requests
 
-url = "https://stack_overflow_data.p.rapidapi.com/xml_data"
+url = "https://stack_overflow_data1.p.rapidapi.com/xml_data"
 
 headers = {
 	"X-RapidAPI-Key": "c12a4fcf8emshf19712541926cdcp19ec27jsn683bd634421e",
-	"X-RapidAPI-Host": "stack_overflow_data.p.rapidapi.com"
+	"X-RapidAPI-Host": "stack_overflow_data1.p.rapidapi.com"
 }
 
 response = requests.request("GET", url, headers=headers)
